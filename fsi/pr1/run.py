20e810d5d3fb9fae41cabd443c1b6579c691bf07
Jesus Santacruz <jesus.santacruz101@alu.ulpgc.es>
2024-12-10 18:21:58 +0000

    Ultima version

--- fsi/pr1/run.py
+++ fsi/pr1/run.py
@@ -25,6 +25,96 @@ print("----------------------------------------------")
 print("Búsqueda con ramificación y acotación con subestimación")
 print(search.branch_and_boundWS_search(ab).path())
 
+print("===============================================================")
+
+oe = search.GPSProblem('O', 'E'
+                       , search.romania)
+
+print("Búsqueda en anchura para OE")
+print(search.breadth_first_graph_search(oe).path())
+
+print("Búsqueda en profundidad para OE")
+print(search.depth_first_graph_search(oe).path())
+
+print("Búsqueda con ramificación y acotación para OE")
+print(search.branch_and_bound_search(oe).path())
+print("-----------------------------------------------------------------")
+print("Búsqueda con ramificación y acotación con subestimación para OE")
+print(search.branch_and_boundWS_search(oe).path())
+
+
+print("===============================================================")
+
+gz = search.GPSProblem('G', 'Z'
+                       , search.romania)
+
+print("Búsqueda en anchura para GZ")
+print(search.breadth_first_graph_search(gz).path())
+
+print("-----------------------------------------------------------------")
+
+print("Búsqueda en profundidad para GZ")
+print(search.depth_first_graph_search(gz).path())
+
+print("-----------------------------------------------------------------")
+
+
+print("Búsqueda con ramificación y acotación para GZ")
+print(search.branch_and_bound_search(gz).path())
+
+print("-----------------------------------------------------------------")
+
+print("Búsqueda con ramificación y acotación con subestimación para GZ")
+print(search.branch_and_boundWS_search(gz).path())
+
+print("===============================================================")
+
+nd = search.GPSProblem('N', 'D'
+                       , search.romania)
+
+print("Búsqueda en anchura para ND")
+print(search.breadth_first_graph_search(nd).path())
+
+print("-----------------------------------------------------------------")
+
+print("Búsqueda en profundidad para ND")
+print(search.depth_first_graph_search(nd).path())
+
+print("-----------------------------------------------------------------")
+
+print("Búsqueda con ramificación y acotación para ND")
+print(search.branch_and_bound_search(nd).path())
+
+print("-----------------------------------------------------------------")
+
+print("Búsqueda con ramificación y acotación con subestimación para ND")
+print(search.branch_and_boundWS_search(nd).path())
+
+print("===============================================================")
+
+mf = search.GPSProblem('M', 'F'
+                       , search.romania)
+
+print("Búsqueda en anchura para MF")
+print(search.breadth_first_graph_search(mf).path())
+
+print("-----------------------------------------------------------------")
+
+print("Búsqueda en profundidad para MF")
+print(search.depth_first_graph_search(mf).path())
+
+print("-----------------------------------------------------------------")
+
+
+print("Búsqueda con ramificación y acotación para MF")
+print(search.branch_and_bound_search(mf).path())
+
+print("-----------------------------------------------------------------")
+
+print("Búsqueda con ramificación y acotación con subestimación para MF")
+print(search.branch_and_boundWS_search(mf).path())
+
+print("===============================================================")
 # Result:
 # [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
 # [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
